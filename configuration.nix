ece86e4df6354bd3554b181d12c8c8f11494a9a0
athomasprojects <amanda.thomas14@gmail.com>
2025-03-05 13:26:19 -0500

    62 current  2025-03-05 13:26:15  25.05.20241217.d3c42f1        6.6.66 - nixos: add experimental pipe operator. ghostty: bump tip version to 1.1.3-66e8d91-nix.

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
     settings = {
       auto-optimise-store = true;
       builders-use-substitutes = true;
-      experimental-features = ["nix-command" "flakes"];
+      experimental-features = ["nix-command" "flakes" "pipe-operators"];
       substituters = [
         "https://nix-community.cachix.org"
       ];
